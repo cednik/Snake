79259a677a3407292a49f8f8456dffcb0e375f43
Kuba Streit <kuba.streit@gmail.com>
2018-03-08 14:00:42 +0100

    add playground - snakes can collide

--- Snake.py
+++ Snake.py
@@ -9,12 +9,15 @@ pygame.init()
 raster = 20
 window_size = (25, 25)
 
+playground = []
+for y in range(window_size[1]):
+    playground.append(window_size[0] * [None])
+
 circle_size = raster // 2 - 1
 
 screen = pygame.display.set_mode((window_size[0] * raster, window_size[1] * raster))
 
 background_color = pygame.Color('black')
-apple_color = pygame.Color('green')
 
 MOVE_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(MOVE_EVENT, 1000//4)
@@ -24,8 +27,35 @@ DOWN  = 3
 LEFT  = 2
 RIGHT = 4
 
+class Apple:
+    def __init__(self, playground):
+        self.playground = playground
+        self.color = pygame.Color('red')
+        self.coor = None
+        self.place()
+
+    def place(self):
+        while True:
+            y = randint(0, len(self.playground)-1)
+            x = randint(0, len(self.playground[y])-1)
+            if (x, y) == self.coor:
+                continue
+            if self.playground[y][x] == None:
+                self.playground[y][x] = self
+                self.coor = (x, y)
+                break
+
+    def eat(self):
+        self.playground[self.coor[1]][self.coor[0]] = None
+        self.place()
+
+    def draw(self, surface):
+        pygame.draw.ellipse(surface, self.color, pygame.Rect(self.coor[0] * raster, self.coor[1] * raster, raster, raster))
+
 class Snake:
-    def __init__(self, start, course, color, keys):
+    def __init__(self, playground, start, course, color, keys):
+        self.playground = playground
+        self.playground[start[1]][start[0]] = self
         self.tail = [start]
         self.course = deque([course])
         self.color = color
@@ -60,21 +90,22 @@ class Snake:
         elif self.course[0] == LEFT:
             x -= 1
         coor = (x, y)
-        if coor in self.tail or x < 0 or y < 0 or x >= window_size[0] or y >= window_size[1]:
+        if x < 0 or y < 0 or y >= len(self.playground) or x >= len(self.playground[y]) or isinstance(self.playground[y][x], Snake):
             self.alive = False
-        self.last = self.tail[-1]
+            for x, y in self.tail:
+                self.playground[y][x] = None
+            return
+        last = self.tail[-1]
         for i in range(len(self.tail) - 1, 0, -1):
             self.tail[i] = self.tail[i-1]
         self.tail[0] = coor
-
-    def check_apple(self, apple):
-        if not self.alive:
-            return
-        if self.tail[0] == apple:
-            self.tail.append(self.last)
+        if isinstance(self.playground[coor[1]][coor[0]], Apple):
+            self.tail.append(last)
             self.score += 1
-            return True
-        return False
+            self.playground[coor[1]][coor[0]].eat()
+        else:
+            self.playground[last[1]][last[0]] = None
+        self.playground[coor[1]][coor[0]] = self
 
     def draw(self, surface):
         if not self.alive:
@@ -86,21 +117,21 @@ class Snake:
     def __bool__(self):
         return self.alive
 
-x = int(window_size[0] / 2)
+snakes = 3
+space = 3
+x0 = int(window_size[0] / 2)
 y = int(window_size[1] / 2)
-start = [(x + 1, y) for x in range(3)]
+start = [(space * x + x0 - space * snakes // 2, y) for x in range(snakes)]
 keys = ((pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d),
         (pygame.K_i, pygame.K_k, pygame.K_j, pygame.K_l),
         (pygame.K_KP8, pygame.K_KP5, pygame.K_KP4, pygame.K_KP6))
 colors = (pygame.color.Color('yellow'),
           pygame.color.Color('cyan'),
           pygame.color.Color('magenta'))
-snakes = [Snake(start[i], UP, color = colors[i], keys = keys[i]) for i in range(3)]
+snakes = [Snake(playground, start[i], UP, color = colors[i], keys = keys[i]) for i in range(snakes)]
 
-x_apple = x
-y_apple = y
+apple = Apple(playground)
 
-add_apple = True
 redraw = True
 
 while True:
@@ -120,26 +151,15 @@ while True:
             snake.move()
         if not any(snakes):
             break
-        for snake in snakes:
-            if snake.check_apple((x_apple, y_apple)):
-                score = ', '.join('{:3}'.format(snake.score) for snake in snakes)
-                pygame.display.set_caption('Snake' + 16 * ' ' + 'Score: {}'.format(score))
-                add_apple = True
-                break
+        pygame.display.set_caption('Snake' + 16 * ' ' + 'Score: {}'.format(', '.join('{:3}'.format(snake.score) for snake in snakes)))
         redraw = True
-        
-    if add_apple:
-        add_apple = False
-        x_apple = randint(0, window_size[0])
-        y_apple = randint(0, window_size[1])
 
     if redraw:
         redraw = False
         screen.fill(background_color)
-        for s in snakes:
-            s.draw(screen)
-        c = (x_apple * raster, y_apple * raster)
-        pygame.draw.ellipse(screen, apple_color, pygame.Rect(c, (raster, raster)))
+        for snake in snakes:
+            snake.draw(screen)
+        apple.draw(screen)
         pygame.display.flip()
 
 pygame.quit()
