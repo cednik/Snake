00e242ab0c2c553db6169965d8dcbc6c7aaceb41
Kuba Streit <kuba.streit@gmail.com>
2018-03-17 14:57:01 +0100

    experiment with vectorization, only partially working

--- levels/extractor.py
+++ levels/extractor.py
@@ -14,10 +14,10 @@ raster = 8
 wall_color = (0, 0, 255, 255)
 
 delta = (
-    ( 0, -1),
     ( 1,  0),
     ( 0,  1),
-    (-1,  0)
+    (-1,  0),
+    ( 0, -1)
 )
 
 rasterize = lambda v: int(ceil(v/raster))
@@ -33,17 +33,20 @@ area = pg.Rect(l, t, w, h)
 
 screen = pg.display.set_mode((w*2, h))
 
-playground = pg.Surface((w, h))
-walls = pg.Surface((w, h))
+playground = pg.Surface((w, h), pg.SRCALPHA)
+walls = pg.Surface((w, h), pg.SRCALPHA)
 
 wall_color_int = walls.map_rgb(wall_color)
 
 quit = False
-pg.time.set_timer(pg.USEREVENT, 100)
+pg.time.set_timer(pg.USEREVENT, 0)
 
-for level in range(1, 31):
+to_raster = lambda l: [[int((p[i] + (0.5, 0.25)[i]) * raster) for i in range(2)] for p in l] 
+
+for level in range(27, 31):
+    pg.display.set_caption('{} / 30'.format(level))
+
     img = pg.image.load('{}.png'.format(level))
-
     playground.blit(img, (0, 0), area)
 
     pg.transform.threshold(walls,          # DestSurface
@@ -80,9 +83,76 @@ for level in range(1, 31):
     screen.blit(playground, (w, 0))
     screen.blit(walls, (0, 0))
 
+    lines = []
+    for y in range(hm):
+        for x in range(wm):
+            if not walls_matrix[y][x]:
+                continue
+            #walls_matrix[y][x] = False
+            line = [(x, y)]
+            pg.draw.circle(screen, pg.Color(0, 255, 0, 192), *to_raster(line), raster // 4)
+            dir = 0
+            turns = 0
+            flipped = False
+            while True:
+                last = (x, y)
+                x += delta[dir][0]
+                y += delta[dir][1]
+                if x < 0 or x == wm or y < 0 or y == hm or not walls_matrix[y][x]:
+                    (x, y) = last
+                    if line[-1] != last:
+                        line.append(last)
+                        pg.draw.circle(screen, pg.Color(255, 0, 255, 192), *to_raster([last]), raster // 4)
+                    else:
+                        pg.draw.circle(screen, pg.Color(255, 255, 0, 192), *to_raster([(x, y)]), raster // 4)
+                    dir += 1
+                    if dir == 4:
+                        dir = 0
+                    turns += 1
+                    if turns == 8:
+                        pg.draw.circle(screen, pg.Color(255, 0, 0, 192), *to_raster([(x, y)]), raster // 4)
+                        if line[0] != line[-1]:
+                            walls_matrix[line[0][1]][line[0][0]] = False
+                        pg.display.flip()
+                        if not flipped:
+                            flipped = True
+                            turns = 0
+                            dir = 1
+                            (x, y) = line[0]
+                            line.reverse()
+                        else:
+                            break
+                else:
+                    walls_matrix[y][x] = False
+                    turns = 0
+                    pg.draw.circle(screen, pg.Color(255, 255, 255, 192), *to_raster([(x, y)]), raster // 4)
+                pg.display.flip()
+                e = pg.event.poll()
+                if e.type == pg.QUIT:
+                    quit = True
+                    break
+                elif e.type == pg.KEYDOWN:
+                    if e.key == pg.K_ESCAPE:
+                        quit = True
+                        break
+                pg.time.wait(200)
+            lines.append(line)
+            if quit: break
+        if quit: break
+    print(len(lines)-1)
+    for l in lines:
+        if len(l) == 1:
+            pg.draw.circle(screen, pg.Color(255, 255, 255, 128), *to_raster(l), raster // 4)
+        else:
+            pg.draw.lines(screen, pg.Color(255, 255, 255, 128), False, to_raster(l), raster // 2)
+    # for p in points:
+    #     pg.draw.circle(walls, pg.Color(255, 0, 0, 128), *to_raster([p]), raster // 4)
+
+    
+
     pg.display.flip()
 
-    while True:
+    while not quit:
         e = pg.event.wait()
         if e.type == pg.QUIT:
             quit = True
@@ -96,9 +166,10 @@ for level in range(1, 31):
         elif e.type == pg.USEREVENT:
             break
         elif e.type == pg.MOUSEMOTION:
-            pg.display.set_caption('[{}, {}]: {}'.format(e.pos[0],
-                                                         e.pos[1],
-                                                         screen.get_at(e.pos)))
+            pg.display.set_caption('{} / 30: [{}, {}]: {}'.format(level,
+                                                                  e.pos[0],
+                                                                  e.pos[1],
+                                                                  screen.get_at(e.pos)))
     if quit:
         break
 
