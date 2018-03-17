1a6327312ed3ed4664b4800a4427cc356f29be78
Kuba Streit <kuba.streit@gmail.com>
2018-03-17 17:01:30 +0100

    add walls

--- Snake.py
+++ Snake.py
@@ -3,29 +3,17 @@ import pygame
 from random import randint
 from math import sqrt
 from collections import deque
+import os
+
+raster = 20
 
 pygame.init()
 
-raster = 20
-window_size = (25, 25)
-
-playground = []
-for y in range(window_size[1]):
-    playground.append(window_size[0] * [None])
-
-circle_size = raster // 2 - 1
-
-screen = pygame.display.set_mode((window_size[0] * raster, window_size[1] * raster))
-
-background_color = pygame.Color('black')
-
-MOVE_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(MOVE_EVENT, 1000//4)
-
-UP    = 1
-DOWN  = 3
-LEFT  = 2
-RIGHT = 4
+class Wall:
+    color = pygame.color.Color('blue')
+        
+    def draw(self, surface, x, y):
+        pygame.draw.rect(surface, Wall.color, pygame.Rect(x, y, raster, raster))
 
 class Apple:
     def __init__(self, playground):
@@ -49,10 +37,15 @@ class Apple:
         self.playground[self.coor[1]][self.coor[0]] = None
         self.place()
 
-    def draw(self, surface):
-        pygame.draw.ellipse(surface, self.color, pygame.Rect(self.coor[0] * raster, self.coor[1] * raster, raster, raster))
+    def draw(self, surface, x, y):
+        pygame.draw.ellipse(surface, self.color, pygame.Rect(x, y, raster, raster))
 
 class Snake:
+    UP    = 1
+    DOWN  = 3
+    LEFT  = 2
+    RIGHT = 4
+    
     def __init__(self, playground, start, course, color, keys):
         self.playground = playground
         self.playground[start[1]][start[0]] = self
@@ -66,14 +59,14 @@ class Snake:
     def control(self, key):
         if not self.alive:
             return
-        if key == self.keys[1] and self.course[-1] != UP:
-            self.course.append(DOWN)
-        elif key == self.keys[0] and self.course[-1] != DOWN:
-            self.course.append(UP)
-        elif key == self.keys[3] and self.course[-1] != LEFT:
-            self.course.append(RIGHT)
-        elif key == self.keys[2] and self.course[-1] != RIGHT:
-            self.course.append(LEFT)
+        if key == self.keys[1] and self.course[-1] != Snake.UP:
+            self.course.append(Snake.DOWN)
+        elif key == self.keys[0] and self.course[-1] != Snake.DOWN:
+            self.course.append(Snake.UP)
+        elif key == self.keys[3] and self.course[-1] != Snake.LEFT:
+            self.course.append(Snake.RIGHT)
+        elif key == self.keys[2] and self.course[-1] != Snake.RIGHT:
+            self.course.append(Snake.LEFT)
 
     def move(self):
         if not self.alive:
@@ -81,16 +74,16 @@ class Snake:
         x, y = self.tail[0]
         if len(self.course) > 1:
             self.course.popleft()
-        if self.course[0] == UP:
+        if self.course[0] == Snake.UP:
             y -= 1
-        elif self.course[0] == DOWN:
+        elif self.course[0] == Snake.DOWN:
             y += 1
-        elif self.course[0] == RIGHT:
+        elif self.course[0] == Snake.RIGHT:
             x += 1
-        elif self.course[0] == LEFT:
+        elif self.course[0] == Snake.LEFT:
             x -= 1
         coor = (x, y)
-        if x < 0 or y < 0 or y >= len(self.playground) or x >= len(self.playground[y]) or isinstance(self.playground[y][x], Snake):
+        if x < 0 or y < 0 or y >= len(self.playground) or x >= len(self.playground[y]) or isinstance(self.playground[y][x], (Snake, Wall)):
             self.alive = False
             for x, y in self.tail:
                 self.playground[y][x] = None
@@ -107,20 +100,52 @@ class Snake:
             self.playground[last[1]][last[0]] = None
         self.playground[coor[1]][coor[0]] = self
 
-    def draw(self, surface):
+    def draw(self, surface, x, y):
         if not self.alive:
             return
-        for coor in self.tail:
-            c = (coor[0] * raster, coor[1] * raster)
-            pygame.draw.rect(screen, self.color, pygame.Rect(c, (raster, raster)))
+        pygame.draw.rect(surface, self.color, pygame.Rect(x, y, raster, raster))
 
     def __bool__(self):
         return self.alive
 
+playground = []
+
+def load_level(name, playground):
+    del playground[:]
+    name = str(name) + '.lvl'
+    items = {
+        ' ': lambda: None,
+        '#': Wall
+        }
+    with open(os.path.join('levels', name)) as f:
+        y = 0
+        for line in f:
+            if line[-1] == '\n':
+                line = line[:-1]
+            if not line:
+                continue
+            row = []
+            for square in line:
+                row.append(None if not square in items else items[square]())
+            playground.append(row)
+
+load_level(1, playground)
+
+circle_size = raster // 2 - 1
+
+h = len(playground)
+w = len(max(playground, key = len))
+screen = pygame.display.set_mode((w * raster, h * raster))
+
+background_color = pygame.Color('black')
+
+MOVE_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(MOVE_EVENT, 1000//4)
+
 snakes = 3
 space = 3
-x0 = int(window_size[0] / 2)
-y = int(window_size[1] / 2)
+y = len(playground) // 2
+x0 = len(playground[y]) // 2
 start = [(space * x + x0 - space * snakes // 2, y) for x in range(snakes)]
 keys = ((pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d),
         (pygame.K_i, pygame.K_k, pygame.K_j, pygame.K_l),
@@ -128,7 +153,7 @@ keys = ((pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d),
 colors = (pygame.color.Color('yellow'),
           pygame.color.Color('cyan'),
           pygame.color.Color('magenta'))
-snakes = [Snake(playground, start[i], UP, color = colors[i], keys = keys[i]) for i in range(snakes)]
+snakes = [Snake(playground, start[i], Snake.UP, color = colors[i], keys = keys[i]) for i in range(snakes)]
 
 apple = Apple(playground)
 
@@ -157,9 +182,14 @@ while True:
     if redraw:
         redraw = False
         screen.fill(background_color)
-        for snake in snakes:
-            snake.draw(screen)
-        apple.draw(screen)
+        y = 0
+        for line in playground:
+            x = 0
+            for square in line:
+                if square != None:
+                    square.draw(screen, x, y)
+                x += raster
+            y += raster
         pygame.display.flip()
 
 pygame.quit()
